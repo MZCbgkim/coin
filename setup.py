7951731186f70cbf53f0696e3b548a62965d6ac6
GreatRoot <bgkim@mz.co.kr>
2023-05-25 16:30:59 +0900

    add python file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='coin',
+  version='1.0.0',
+  description='Im very hungri',
+  author='bgkim',
+  author_email='bgkim@mz.co.kr',
+  url='https://github.com/MZCbgkim/coin.git',
+  packages=find_packages(),
+  python_requires='>=3.4'
+)
